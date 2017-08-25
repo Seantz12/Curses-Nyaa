2452b04941be1ae3ad56fbb046f7893729f13cda
Sean Zhou <seantxzhou12@gmail.com>
2017-08-25 18:40:35 -0400

    Updated to have working interface, see readme for details

--- main_screen.py
+++ main_screen.py
@@ -9,29 +9,71 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 def main(stdscr):
-    # Make stdscr.getch non-blocking
-    preferences = nyaa_linker.read_settings()
     stdscr.clear()
     width = 4
     count = 0
     direction = 1
-    while True:
-    	anime = get_input(stdscr, 0, 0, "Please type in a search term").lower()
-    	stdscr.addstr(3, 0, nyaa_linker.return_torrents(preferences, anime))
-        c = stdscr.getch()
-        if c == ord('q'):
-        	quit()
-        else:
-        	stdscr.clear()
+    
+    page = 1
+    webpage = 1
+    need_input = 'True'
+    running = True
 
-def get_input(stdscr, r, c, prompt_string):
-	curses.echo()
-	stdscr.addstr(r, c, prompt_string)
-	stdscr.refresh()
-	input = stdscr.getstr(r + 1, c, 20)
-	return input
+    while running:
+        if(need_input == 'True'):
+            anime = get_input(stdscr, 0, 0, "Please type in a search term").lower() # Prompts user for input
+            all_results = nyaa_linker.return_torrents(read_settings(), anime, webpage) # Actually grabs torrent list from Nyaa
+        torrent_window = stdscr.subwin(4, 0)
+        torrent_window.addstr(0, 0, list_torrents(all_results, page)) # Displays the five entries
+        stdscr.refresh()
+        torrent_window.refresh()
 
-def quit():
-	curses.endwin()
+        c = torrent_window.getch()
+        if c == ord('q'): # Quits the program
+            curses.endwin()
+            running = False
+        elif c == ord('n'): # Advances to the next page
+            page += 1
+            torrent_window.clear()
+            need_input = 'False'
+        elif c == ord('p') and page != 1: # Goes back to previous page
+            page -= 1
+            torrent_window.clear()
+            need_input = 'False'
+        elif c == ord('i'): # Takes in a new input
+            torrent_window.clear()
+            stdscr.clear()
+            need_input = 'True'
+            page = 1
+
+def get_input(stdscr, r, c, prompt_string): # Long story short, prompts user for input
+    curses.echo()
+    stdscr.addstr(r, c, prompt_string)
+    stdscr.refresh()
+    input = stdscr.getstr(r + 1, c, 20)
+    return input
+
+def list_torrents(all_torrents, section): # Converts lists to a string to be displayed easily
+    string = ''
+    lower_bound = (section-1)*5 # Makes the index ranges (0-4, 5-9, etc.)
+    if(isinstance(all_torrents, basestring)): # This only happens when one result, or error result so yeah
+        string += all_torrents
+    elif(len(all_torrents) < lower_bound + 5): # In case we reach the end, doesn't go over bounds
+        for index in range(lower_bound, len(all_torrents)):
+            string += all_torrents[index]
+    else:
+        for index in range(lower_bound, (lower_bound + 5)):
+            string += all_torrents[index]
+    return string
+
+def read_settings(): # Reads the settings from the file
+    settings = ['', '', 'false']
+    file = open("settings", "r")
+    types = file.readlines()
+    for i in range(3):
+        temp = types[i]
+        temp = temp[temp.index('=')+1:]
+        settings[i] = temp.rstrip('\n')
+    return settings
 
 wrapper(main)
--- nyaa_linker.py
+++ nyaa_linker.py
@@ -1,23 +1,16 @@
 # TODO:
 # Add a filter for anime only and other shit, subs/raws/whatever the hell else there is
-# Create an actual interface (curses or gtk or whatever)
 # Make code look neater
 # Publish that shit yo
 from bs4 import BeautifulSoup
 import urllib, urllib2
 
-settings = ['', '', 'false']
-
-def main():
-	read_settings()
-	anime = raw_input('ay yo whatchu looking for: ')
-	anime = urllib.quote_plus(anime)
-	return_torrents(settings, anime)
-
-def return_torrents(prefernces, anime_name):
+def return_torrents(prefernces, anime_name, webpage):
 	# Opens up the website with the search term
 	# url format https://nyaa.si/?f=0&c=0_0&q=ANIME_NAME&s=SORTING&o=ACCES/DESC
-	website = "https://nyaa.si/?f=0&c=0_0&q=" + anime_name + "&s=" + prefernces[0] + "&o=" + prefernces[1]
+	website = "https://nyaa.si/?f=0&c=0_0&q=" + \
+			   anime_name + "&s=" + prefernces[0] + \
+			   "&o=" + prefernces[1] + "&p=" + str(webpage)
 	req = urllib2.Request(website, headers={'User-Agent' : "Magic Browser"})
 	page = urllib2.urlopen(req)
 
@@ -30,19 +23,21 @@ def return_torrents(prefernces, anime_name):
 	except AttributeError:
 		return "\nNo results found, try a different search term\n"
 
-	results = ''
+	results = []
 	for row in torrent_list.findAll("tr"): # iterates through each torrent row
 		quality = row.get('class')[0]
 		quality[4:-2] # gets rid of the unneeded bits (['u']) part
-
+		torrent_info = ''
 		if(prefernces[2] == 'false' or quality == 'success'):
-			results += return_category(row) + '\n'
-			results += return_torrent_title(row) + '\n'
-			results += return_seeder_leecher_count(row) + '\n\n'
+			torrent_info += return_category(row) + '\n'
+			torrent_info += return_torrent_title(row) + '\n'
+			torrent_info += return_seeder_leecher_count(row) + '\n\n'
+			results.append(torrent_info)
 
-	if results == '':
+	if len(results) == 0:
 		return 'No results found, try changing your search filters'
 	else:
+		results.append('End of page')
 		return results
 
 def return_torrent_title(row):
@@ -69,16 +64,3 @@ def return_category(row):
 	return category.get('title')
 
 #def return_magnent_link(row):
-
-
-def read_settings():
-	file = open("settings", "r")
-	types = file.readlines()
-	for i in range(3):
-		temp = types[i]
-		temp = temp[temp.index('=')+1:]
-		settings[i] = temp.rstrip('\n')
-	return settings
-
-if __name__ == '__main__':
-	main()
