43102dc0f407bb151e3ff2d42ea18b3501cd6ff4
Sean Zhou <seantxzhou12@gmail.com>
2017-08-27 14:36:10 -0400

    Can download torrents with transmission-gtk

--- main_screen.py
+++ main_screen.py
@@ -3,33 +3,39 @@ from curses import wrapper
 import nyaa_linker
 import time
 import sys
+import os
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 def main(stdscr):
     stdscr.clear()
     curses.curs_set(0)
+    # Creating the windows before hand for where everything goes
+    entry_window = stdscr.subwin(2, 0)
+    torrent_window = stdscr.subwin(5, 0)
+    #info_window = stdscr.subwin(50, 0)
+
+    #curses.resizeterm(50, 75)
     # Variables are pretty self-explanatory
     page = 1
     webpage = 1
     last_webpage = 0
+    torrent_client = 'transmission-gtk '
+    terminal_client = 'gnome-terminal '
     anime = ''
     running = True
 
+
     while running:
-
-        stdscr.addstr(0, 0, "Welcome to the Nyaa Scraper! Press i to begin") # Welcome message
+        stdscr.addstr(0, 0, "Nyaa torrent downloader 1.0") # Welcome message
         stdscr.refresh()
-
-        # Creating the windows before hand for where everything goes
-        entry_window = stdscr.subwin(2, 0)
-        torrent_window = stdscr.subwin(5, 0)
-        info_window = stdscr.subwin(35, 0)
 
         # Gets an entry and populates torrent list with it, only after user presses right button
         if(anime != ''):
             if webpage != last_webpage: # If needed to advance webpage on nyaa itself
-                all_results = nyaa_linker.return_torrents(read_settings(), anime, webpage)
+                everything = nyaa_linker.return_torrents(read_settings(), anime, webpage)
+                all_results = everything[0]
+                magnet_links = everything[1]
                 last_webpage += 1
                 # TEMP FIX, tired from coding, will fix later
                 if(last_webpage == webpage + 2):
@@ -38,7 +44,7 @@ def main(stdscr):
             torrent_window.addstr(0, 0, entries) # Displays the five entries
             torrent_window.refresh()
 
-        info_window.addstr("i to search entries, j to go to next page, k to go to previous page, q to quit")
+        #info_window.addstr("i to search entries, j to go to next page, k to go to previous page, q to quit")
         curses.noecho()
         c = entry_window.getch()
         if c == ord('i'): # Takes in a new input
@@ -58,14 +64,30 @@ def main(stdscr):
             if page == 16: # Nyaa has maximum 75 entries, therefore after the 15th page it needs to load something new
                 webpage += 1
                 page = 1
-            #need_input = 'False'
         elif c == ord('k') and anime != '' and not(page == 1 and webpage == 1): # Goes back to previous page
             page -= 1
             if page == 0:
                 webpage -= 1
                 page = 15
             torrent_window.clear()
-            #need_input = 'False'
+        elif c == ord('d') and anime != '':
+            curses.curs_set(1)
+            # FIX cannot clear torrent selection line, wtf
+            while True:
+                selection = get_input(torrent_window, 38, 0, "Enter a number between 1-5" +\
+                    " (1 for top torrent, 5 for bottom).") #Prompts user selection
+                try:
+                    if int(selection) <= 5 and int(selection) >= 1:
+                        #torrent_window.refresh()
+                        magnet_index = (page-1)*5 + (int(selection)-1)
+                        os.system(torrent_client + magnet_links[magnet_index])
+                        torrent_window.clear()
+                        break
+                    else:
+                        torrent_window.addstr(37, 0, "Please enter a number between 1-5")
+                except ValueError:
+                    torrent_window.addstr(37, 0, "Please enter a number between 1-5")
+            torrent_window.clear() # temp fix, just testing out
         elif c == ord('q'): # Quits the program
             curses.endwin()
             running = False
@@ -82,7 +104,7 @@ def list_torrents(all_torrents, section): # Converts lists to a string to be dis
     lower_bound = (section-1)*5 # Makes the index ranges (0-4, 5-9, etc.)
     if isinstance(all_torrents, basestring): # This only happens when one result, or error result so yeah
         string += all_torrents
-    elif(len(all_torrents) < lower_bound + 5): # In case we reach the end, doesn't go over bounds
+    elif len(all_torrents) < lower_bound + 5: # In case we reach the end, doesn't go over bounds
         for index in range(lower_bound, len(all_torrents)):
             string += all_torrents[index]
     else:
@@ -102,5 +124,8 @@ def read_settings(): # Reads the settings from the file
         temp = temp[temp.index('=')+1:]
         settings[i] = temp.rstrip('\n')
     return settings
+
+def write_settings():
+    file = open("settings", "w")
 
 wrapper(main)
--- nyaa_linker.py
+++ nyaa_linker.py
@@ -7,8 +7,8 @@ def return_torrents(prefernces, anime_name, webpage):
 	website = "https://nyaa.si/?f=0&c=0_0&q=" + \
 			   anime_name + "&s=" + prefernces[0] + \
 			   "&o=" + prefernces[1] + "&p=" + str(webpage)
-	req = urllib2.Request(website, headers={'User-Agent' : "Magic Browser"})
-	page = urllib2.urlopen(req)
+	
+	page = retry(website)
 
 	soup = BeautifulSoup(page, "html.parser")
 
@@ -20,6 +20,7 @@ def return_torrents(prefernces, anime_name, webpage):
 		return "\nNo results found, try a different search term\n"
 
 	results = []
+	magnet_links = []
 	for row in torrent_list.findAll("tr"): # iterates through each torrent row
 		quality = row.get('class')[0]
 		quality[4:-2] # gets rid of the unneeded bits (['u']) part
@@ -29,11 +30,19 @@ def return_torrents(prefernces, anime_name, webpage):
 			torrent_info += return_torrent_title(row) + '\n'
 			torrent_info += return_seeder_leecher_count(row) + '\n\n'
 			results.append(torrent_info)
+			magnet_links.append(return_magnet_link(row))
 
 	if len(results) == 0:
 		return 'No results found, try changing your search filters'
 	else:
-		return results
+		return results, magnet_links
+
+def retry(url): # Keeps at it until you get something
+	try:
+		req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
+		return urllib2.urlopen(req)
+	except urllib2.HTTPError:
+		retry(url)
 
 def return_torrent_title(row):
 	element = row.findAll('td') # groups individual elements of the row
@@ -58,4 +67,8 @@ def return_category(row):
 	category = element.find('a')
 	return category.get('title')
 
-#def return_magnent_link(row):
+def return_magnet_link(row):
+	element = row.findAll('td')
+	links = element[2].findAll('a')
+	return links[1].get('href')
+
